172944dce8e81eb364cfdd1acb8c1d0b8bf6c6b9
HabibMollah <hrmollah2002@gmail.com>
2023-06-08 01:52:45 +0600

    add margins and paddings

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,19 @@
 const Form = () => {
   return (
-    <form className="form-control">
+    <form className="form-control mx-4 md:mx-16 xl:px-96">
       <label className="label" htmlFor="title">
         Title
       </label>
-      <input className="input input-bordered" type="text" id="title" />
+      <input className="input-bordered input" type="text" id="title" />
       <label className="label" htmlFor="description">
         Description
       </label>
-      <input className="input input-bordered" type="text" id="description" />
+      <input className="input-bordered input" type="text" id="description" />
       <label className="label" htmlFor="due-date">
         Due Date
       </label>
-      <input className="input input-bordered" type="date" id="due-date" />
-      <button className="btn btn-outline my-8" type="submit">
+      <input className="input-bordered input" type="date" id="due-date" />
+      <button className="btn-outline btn my-8" type="submit">
         Add Task
       </button>
     </form>
