7a9a11bfbe5b63c6f6245737b71754c1c2d69f2e
HabibMollah <hrmollah2002@gmail.com>
2023-06-08 01:42:57 +0600

    add margin to heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
-import NotificationsPage from './components/NotificationsPage';
-import TasksPage from './components/TasksPage';
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import NotificationsPage from "./components/NotificationsPage";
+import TasksPage from "./components/TasksPage";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold text-center">TaskHub</h1>
+      <h1 className="mt-4 text-center text-5xl font-bold">TaskHub</h1>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="notifications" element={<NotificationsPage />} />
